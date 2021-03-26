113f54194e2b215554060657d5805dc62f631cc1
Alex <wbyun1461@gmail.com>
2021-03-26 09:07:01 +0900

    first commit of spring-webservice2

---
+++ src/test/java/com/jojoldu/springwebservice2/SpringWebservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.springwebservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
