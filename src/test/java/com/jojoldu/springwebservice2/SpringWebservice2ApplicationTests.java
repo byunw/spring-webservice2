82745aa9afdd1adb6bf65a70ba72f8dcab21ba9c
Alex <wbyun1461@gmail.com>
2021-04-06 10:16:09 +0900

    Added @SpringBootTest(classes=Application.class) inside SpringWebservice2ApplicationTests.java

--- src/test/java/com/jojoldu/springwebservice2/SpringWebservice2ApplicationTests.java
+++ src/test/java/com/jojoldu/springwebservice2/SpringWebservice2ApplicationTests.java
@@ -1,11 +1,12 @@
 package com.jojoldu.springwebservice2;
 
+import com.jojoldu.webservice2.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=Application.class)
 class SpringWebservice2ApplicationTests {
-
+    
     @Test
     void contextLoads() {
     }
