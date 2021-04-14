0359f66e746b9922b05aa955c0d73d3f5f44a61e
Alex <wbyun1461@gmail.com>
2021-04-14 14:38:46 +0900

    write_post function inside WebController.java  now returns “page/writepostpage” instead of “writepostpage” since subfolder page was created inside resources/templates to hold writepostpage.html.

--- src/main/java/com/jojoldu/webservice2/web/WebController.java
+++ src/main/java/com/jojoldu/webservice2/web/WebController.java
@@ -13,9 +13,7 @@ import javax.servlet.http.HttpSession;
 public class WebController{
 
     private PostsRepository postrepository;
-
-
-
+    
     @GetMapping("/delete/{id}")
     public String deletepost(@PathVariable Long id){
         postrepository.deleteById(id);
@@ -40,11 +38,11 @@ public class WebController{
 
         //로그인 하지않은 경우: ex, 홈페이지에서 바로 게시글 작성 버튼누르는 경우
         if(sessionedUser==null){
-            return "loginpage";
+            return "page/loginpage";
         }
 
         //로그인 성공한경우(회원가입할떄 작성한 사용자아이디/패스워드를 로그인시 작성한경우),게시글 작성 버튼눌렀을때 게시글 작성 페이지 보여주기
-        return "writepostpage";
+        return "page/writepostpage";
     }
 
     @PostMapping("/savepost")
