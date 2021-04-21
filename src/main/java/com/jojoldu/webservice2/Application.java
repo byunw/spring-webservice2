6adad2b652fec02ce44cc8a36e5d6715ddac3fb0
Alex <wbyun1461@gmail.com>
2021-04-21 15:27:06 +0900

    서버 실행시 real-application.yml 가져와서 읽게 설정하기

--- src/main/java/com/jojoldu/webservice2/Application.java
+++ src/main/java/com/jojoldu/webservice2/Application.java
@@ -2,13 +2,22 @@ package com.jojoldu.webservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application{
+
+    public static final String APPLICATION_LOCATIONS="spring.config.location="
+            +"classpath:application.yml,"+"src/main/resources/real-application.yml";
+            
+
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
 
