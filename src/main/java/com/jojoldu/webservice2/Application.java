07530f4e3495d4debec9cb21f852d56187aff793
Alex <wbyun1461@gmail.com>
2021-04-21 16:10:58 +0900

    changed the path of application.yml inside application.java

--- src/main/java/com/jojoldu/webservice2/Application.java
+++ src/main/java/com/jojoldu/webservice2/Application.java
@@ -1,6 +1,5 @@
 package com.jojoldu.webservice2;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application{
 
     public static final String APPLICATION_LOCATIONS="spring.config.location="
-            +"classpath:application.yml,"+"src/main/resources/real-application.yml";
+            +"classpath:src/main/resources/application.yml,"+"src/main/resources/real-application.yml";
 
 
     public static void main(String[] args){
@@ -18,6 +17,7 @@ public class Application{
         new SpringApplicationBuilder(Application.class)
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
+                
     }
 }
 
