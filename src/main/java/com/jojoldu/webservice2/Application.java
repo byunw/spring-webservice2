ac929f87a584bed3aee5c3ceec06cda76082e5de
Alex <wbyun1461@gmail.com>
2021-04-22 14:12:51 +0900

    rolled back spring-webservice2/src/main/java/com/jojoldu/webservice2/Application.java

--- src/main/java/com/jojoldu/webservice2/Application.java
+++ src/main/java/com/jojoldu/webservice2/Application.java
@@ -1,30 +1,16 @@
 package com.jojoldu.webservice2;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application{
 
-    public static final String APPLICATION_LOCATIONS="spring.config.location="
-            +"classpath:application.yml,"+"/spring-webservice2/real-application.yml";
-
     public static void main(String[] args){
-
-        new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-
+        SpringApplication.run(Application.class, args);
     }
-
+    
 }
-
-
-
-
-
-
-
 
