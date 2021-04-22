852da66daf5270024cbec292e0bb6888bca6d49f
Alex <wbyun1461@gmail.com>
2021-04-22 09:58:27 +0900

    made application.yml to include real-db profile & the value of APPLICATION_LOCATIONS is adjusted to the actual location of real-application inside the linux server

--- src/main/java/com/jojoldu/webservice2/Application.java
+++ src/main/java/com/jojoldu/webservice2/Application.java
@@ -10,7 +10,7 @@ public class Application{
 
     public static final String APPLICATION_LOCATIONS="spring.config.location="
             +"classpath:application.yml,"+"/spring-webservice2/real-application.yml";
-            
+
     public static void main(String[] args){
 
         new SpringApplicationBuilder(Application.class)
