53834cb16a7693dc30195fdc401206b8de2c0431
Alex <wbyun1461@gmail.com>
2021-04-22 09:44:56 +0900

    Application.java is configured to “/spring-webservice2/real-application.yml”

--- src/main/java/com/jojoldu/webservice2/Application.java
+++ src/main/java/com/jojoldu/webservice2/Application.java
@@ -4,14 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@SpringBootApplication
 public class Application{
 
     public static final String APPLICATION_LOCATIONS="spring.config.location="
-            +"classpath:src/main/resources/application.yml,"+"src/main/resources/real-application.yml";
-
-
+            +"classpath:application.yml,"+"/spring-webservice2/real-application.yml";
+            
     public static void main(String[] args){
 
         new SpringApplicationBuilder(Application.class)
