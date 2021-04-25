aba07d30a3588e250ee095e2b99cec2e56980617
Alex <wbyun1461@gmail.com>
2021-04-25 10:51:42 +0900

    회원가입기능이 제대로 작동안해서 User.java에서 constructor 없애고 setter method 생성

--- src/main/java/com/jojoldu/webservice2/domain/Users/User.java
+++ src/main/java/com/jojoldu/webservice2/domain/Users/User.java
@@ -2,6 +2,7 @@ package com.jojoldu.webservice2.domain.Users;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,11 +23,20 @@ public class User{
     private String name;
     private String email;
 
-    public User(String userId,String password,String name,String email){
-        this.userId=userId;
-        this.password=password;
-        this.name=name;
-        this.email=email;
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
     }
 
     @Override
