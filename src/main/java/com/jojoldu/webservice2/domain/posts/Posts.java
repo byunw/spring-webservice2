590f8acb7e07d362e8afff1a4da516064bf22cbf
Alex <wbyun1461@gmail.com>
2021-04-27 11:18:48 +0900

    got rid of referencedColumnName attribute inside @JoinColumn

--- src/main/java/com/jojoldu/webservice2/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/webservice2/domain/posts/Posts.java
@@ -24,8 +24,9 @@ public class Posts extends BaseTimeEntity{
     private String content;
 
     @ManyToOne
-    @JoinColumn(name="author_id",referencedColumnName="id")
+    @JoinColumn(name="author_id")
     private User author;
+
     
     public Posts(String title,String content,User author){
         this.title=title;
