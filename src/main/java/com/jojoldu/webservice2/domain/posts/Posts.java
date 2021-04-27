3e361c0cdd7da4d9febf995c28bb8659a4ecd157
Alex <wbyun1461@gmail.com>
2021-04-27 09:40:30 +0900

    now @JoinColumn(name=“author_id”,referencedColumnName=“id”) where id is the primary key of User table

--- src/main/java/com/jojoldu/webservice2/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/webservice2/domain/posts/Posts.java
@@ -24,8 +24,9 @@ public class Posts extends BaseTimeEntity{
     private String content;
 
     @ManyToOne
-    @JoinColumn(name="author_id",referencedColumnName="userId")
+    @JoinColumn(name="author_id",referencedColumnName="id")
     private User author;
+    
 
     public Posts(String title,String content){
         this.title=title;
