ce4cef561b86a0f748d18d9aae0e66a10f11efda
Alex <wbyun1461@gmail.com>
2021-05-02 15:07:02 +0900

    added @DateTimeFormat(pattern=“yyyy-MM-dd”) for both createDate and modifiedDate

--- src/main/java/com/jojoldu/webservice2/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/webservice2/domain/BaseTimeEntity.java
@@ -3,6 +3,8 @@ package com.jojoldu.webservice2.domain;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -10,16 +12,16 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity{
-
+    
     //automatically creates date value for create_date column
     @CreatedDate
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDateTime createdDate;
 
     //automatically creates data value for modifiedDate column
     @LastModifiedDate
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDateTime modifiedDate;
-
-
 
 
 }
