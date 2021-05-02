28470eec816a8cfb231361db6a9b42e242090b44
Alex <wbyun1461@gmail.com>
2021-05-03 08:36:55 +0900

    rolling back BaseTimeEntity.java

--- src/main/java/com/jojoldu/webservice2/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/webservice2/domain/BaseTimeEntity.java
@@ -3,8 +3,6 @@ package com.jojoldu.webservice2.domain;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -15,12 +13,10 @@ public abstract class BaseTimeEntity{
     
     //automatically creates date value for create_date column
     @CreatedDate
-    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDateTime createdDate;
 
     //automatically creates data value for modifiedDate column
     @LastModifiedDate
-    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDateTime modifiedDate;
 
 
