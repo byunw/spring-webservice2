2f9f6c793bc02ba6d901f878473636078e827ddb
Alex <wbyun1461@gmail.com>
2021-05-18 11:26:02 +0900

    adding /test handler inside UserController.java not WebRestController.java

--- src/main/java/com/jojoldu/webservice2/web/UserController.java
+++ src/main/java/com/jojoldu/webservice2/web/UserController.java
@@ -14,12 +14,16 @@ public class UserController{
 
     private UsersRepository usersRepository;
 
+    //회원가입기능
     @PostMapping("/create")
     public String create_save(User user){
 
         try{
             usersRepository.save(user);
         }
+
+        //이미 다른사람이 사용한 사용자아이디로 회원가입하려면 에러뜰고
+        //catch부분안에있는 return "page/trydifferentid"이 실행이됨
 
         catch (Exception e){
             return "page/trydifferentid";
@@ -29,6 +33,7 @@ public class UserController{
 
     }
 
+    //회원가입페이지 보여줌
     @GetMapping("/users/form")
     public String register_form(){
         return "page/registerform";
@@ -44,15 +49,18 @@ public class UserController{
 
         User user=usersRepository.findByUserId(userId);
 
+        //로그인할때 타입한 사용자아이디가 user table에 존재하지않는경우
         if(user==null){
             return "page/idnotexist";
         }
 
+        //해당사용자아이디의 패스워드랑 로그인할당시에 친 패스워드랑 같은경우
         if(user.getPassword().equals(password)){
             session.setAttribute("sessioneduser",user);
             return "redirect:/";
         }
 
+        //해당 사용자아이디의 비빌번호랑 유저가 친 비빌번호가 다를경우
         if(!user.getPassword().equals(password)){
             return "page/retypepassword";
         }
@@ -67,6 +75,11 @@ public class UserController{
         session.removeAttribute("sessioneduser");
         return "redirect:/";
 
+    }
+
+    @GetMapping("/test")
+    public String show(){
+        return "page/test";
     }
 
 
