13b35be9d2a7944a3f158a8a15b47e81ff03483a
Alex <wbyun1461@gmail.com>
2021-05-22 13:28:04 +0900

    원격서버에 CommentRepository.java 추가

---
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package com.jojoldu.webservice2.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+}
