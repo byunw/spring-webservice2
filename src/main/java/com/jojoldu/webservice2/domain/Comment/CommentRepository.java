0c9a83e446d312e79a9e60c84d8b2f67cf55dfa2
Alex <wbyun1461@gmail.com>
2021-05-23 22:10:20 +0900

    added @Transactional inside CommentRepository.java

--- src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -2,16 +2,13 @@ package com.jojoldu.webservice2.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import javax.transaction.Transactional;
 import java.util.stream.Stream;
 
+@Transactional
 public interface CommentRepository extends JpaRepository<Comment,Long>{
-
-    //jpql syntax 틀린거같음
-    //join 사용해야되나?
 
     @Query(value= "select c FROM Comment c where c.toppost= :id")
     Stream<Comment> findAllCommentforeachpost(@Param("id") Long id);
-
 
 }
