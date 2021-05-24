c41f091f524caca9ea618295243278c26d054997
Alex <wbyun1461@gmail.com>
2021-05-24 11:51:07 +0900

    findAllCommentforeachpost() returns List<Object[]> since @Query is used

--- src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -2,11 +2,13 @@ package com.jojoldu.webservice2.domain.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
 
-    @Query(value="SELECT c FROM Comment c where c.toppost= ?1")
-    List<Comment> findAllCommentforeachpost(Long id);
+    //문제는 line 11,12번째에있음
+    @Query(value="SELECT c FROM Comment c where c.toppost= :id")
+    List<Object[]> findAllCommentforeachpost(@Param("id") Long id);
     
 }
