3657950d786e1d8a57fb12c387cb2dd861bd0d6c
Alex <wbyun1461@gmail.com>
2021-05-24 09:31:25 +0900

    deleted @param inside findAllCommentforeachpost

--- src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -1,14 +1,13 @@
 package com.jojoldu.webservice2.domain.Comment;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import javax.transaction.Transactional;
 import java.util.stream.Stream;
 
-@Transactional
 public interface CommentRepository extends JpaRepository<Comment,Long>{
 
     @Query(value= "select c FROM Comment c where c.toppost= :id")
-    Stream<Comment> findAllCommentforeachpost(@Param("id") Long id);
-
+    Stream<Comment> findAllCommentforeachpost(Long id);
+    
 }
