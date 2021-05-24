af527762a4389c912ebcf1f8c4c7881bca5e0db9
Alex <wbyun1461@gmail.com>
2021-05-24 10:58:23 +0900

    changed the syntax of jpql inside CommentRepository.java

--- src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -2,13 +2,11 @@ package com.jojoldu.webservice2.domain.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
-
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
 
-    @Query("SELECT c FROM Comment c where c.toppost= ?1")
+    @Query(value="SELECT c FROM Comment c where c.toppost= ?1")
     List<Comment> findAllCommentforeachpost(Long id);
     
 }
