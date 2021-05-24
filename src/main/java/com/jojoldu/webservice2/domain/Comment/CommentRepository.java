0e267ae2e11efe0aaaa7b548140958cb65744fec
Alex <wbyun1461@gmail.com>
2021-05-24 20:38:46 +0900

    changed jpql inside CommentRepository

--- src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
+++ src/main/java/com/jojoldu/webservice2/domain/Comment/CommentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
 
-        @Query(value="SELECT c FROM Comment c where c.toppost= :id")
+        @Query(value="SELECT c FROM Comment c where c.post_id= :id")
         List<Comment> findAllCommentforeachpost(@Param("id") Long id);
-
+        
 }
